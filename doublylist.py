f10bcd126ebe9d15af735e810c6a28d7a0bbafb2
xoaeb <shoaib.salim@npstx.com>
2024-05-20 11:58:11 +0530

    "Added pop method to DoublyList class and modified test case to include additional append and pop operations."

--- doublylist.py
+++ doublylist.py
@@ -31,8 +31,40 @@ class DoublyList:
         self.length += 1
         return True
 
+    # def pop(self):
+    #     if self.length == 0:
+    #         return None
+    #     if self.length == 1:
+    #         return self.head
+    #     else:
+    #         temp = self.tail
+    #         self.tail = self.tail.prev
+    #         self.tail.next = None
+    #         temp.prev = None
+    #         self.length-=1
+    #         return temp.value
+    #     return True
+
+    # better solution 👇👇
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+
+        self.length -= 1
+        return temp
+
 
 my_doublylist = DoublyList(3)
 my_doublylist.append(9)
-
+my_doublylist.append(71)
+print(my_doublylist.pop())
 my_doublylist.printList()
