304a275cf9fb934bf1a43f7bfa6c99fc902189fb
xoaeb <shoaib100aib@gmail.com>
2025-02-16 18:27:54 +0530

    error handling

--- graph.py
+++ graph.py
@@ -10,7 +10,7 @@ class Graph:
             
     def print_graph(self):
         for v in self.adj_list:
-            print(v, 'vertex points to  ', self.adj_list[v])
+            print(v, ':  ', self.adj_list[v])
             
             
     def add_edge(self, v1, v2):
@@ -22,8 +22,11 @@ class Graph:
     
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].remove(v2)
-            self.adj_list[v2].remove(v1)
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
             return True
         return False
             
@@ -34,6 +37,8 @@ my_graph =  Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
 my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
 
 
 my_graph.print_graph()
@@ -43,7 +48,7 @@ my_graph.add_edge('B', 'C')
 my_graph.add_edge('C', 'A')
 
 
-my_graph.remove_edge('B', 'C')
+my_graph.remove_edge('B', 'D')
 
 my_graph.print_graph()
 
