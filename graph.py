10c1ece60f21a00b8088907e286638a8dbcd5e43
xoaeb <shoaib100aib@gmail.com>
2025-02-16 18:24:57 +0530

    remove edge fn

--- graph.py
+++ graph.py
@@ -10,13 +10,22 @@ class Graph:
             
     def print_graph(self):
         for v in self.adj_list:
-            print(v, 'vertex is ', self.adj_list[v])
+            print(v, 'vertex points to  ', self.adj_list[v])
             
             
     def add_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+        return False
             
             
             
@@ -24,10 +33,17 @@ my_graph =  Graph()
 
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+
 
 my_graph.print_graph()
 
 my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+
+
+my_graph.remove_edge('B', 'C')
 
 my_graph.print_graph()
 
